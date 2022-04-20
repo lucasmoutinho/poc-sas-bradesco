7729688b498045601667d43a840455fe28889d46
Lucas Moutinho <lucas.silvamoutinho@gmail.com>
2022-04-20 16:24:54 -0400

    Update resolve_json_bradesco.py

--- sid/resolve_json_bradesco.py
+++ sid/resolve_json_bradesco.py
@@ -11,8 +11,6 @@ def execute (json_entrada): # noqa
     'Output: guia, anexo_exame, anexo_laudo, situacao_financeira_cartao, situacao_cadastro_cartao, situacao_cadastro_referenciado, nome_beneficiario, cartao_beneficiario, codigo_procedimento, descricao_procedimento, codigo_referenciado, cnpj_referenciado, nome_referenciado, tabela_procedimento'
     j = json.loads(json_entrada)
     guia = j["guia"]
-    anexo_exame = retorna_booleano(j["anexo_exame"])
-    anexo_laudo = retorna_booleano(j["anexo_laudo"])
     situacao_financeira_cartao = j["situacao_financeira_cartao"]
     situacao_cadastro_cartao = j["situacao_cadastro_cartao"]
     situacao_cadastro_referenciado = j["situacao_cadastro_referenciado"]
@@ -24,5 +22,7 @@ def execute (json_entrada): # noqa
     cnpj_referenciado = j["cnpj_referenciado"]
     nome_referenciado = j["nome_referenciado"]
     tabela_procedimento = j["tabela_procedimento"]
+    anexo_exame = retorna_booleano(j["anexo_exame"])
+    anexo_laudo = retorna_booleano(j["anexo_laudo"])
 
     return guia, anexo_exame, anexo_laudo, situacao_financeira_cartao, situacao_cadastro_cartao, situacao_cadastro_referenciado, nome_beneficiario, cartao_beneficiario, codigo_procedimento, descricao_procedimento, codigo_referenciado, cnpj_referenciado, nome_referenciado, tabela_procedimento
