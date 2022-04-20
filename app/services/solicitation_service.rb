4f640d223fe7a57cbe3b37ab03d1e147af6eff2a
lucasmoutinho <lucasmoutinho@github.com>
2022-04-20 18:07:24 -0300

    :rocket: add: simulating json response

--- app/services/solicitation_service.rb
+++ app/services/solicitation_service.rb
@@ -1,5 +1,6 @@
 require 'httparty'
 require 'json'
+require 'time'
 
 class SolicitationService
     def initialize(solicitation)
@@ -13,6 +14,7 @@ class SolicitationService
     def update_solicitation_api
         analise_medica, analise_adm, liberacao_automatica, resultado_solicitacao = self.call_sas_api
         @solicitation.update(automatic_release: liberacao_automatica, adm_analysis: analise_adm, medic_analysis: analise_medica, result: resultado_solicitacao)
+        self.simulate_json
     end
 
     def call_sas_api
@@ -106,6 +108,55 @@ class SolicitationService
         end
 
         return analise_medica, analise_adm, liberacao_automatica, resultado_solicitacao
+    end
+
+    def simulate_json
+        ae_rand= "AE" + Time.now.utc.to_formatted_s(:number) + rand(17..999999).to_s 
+        sc_rand= "SC" + Time.now.utc.to_formatted_s(:number) + rand(17..999999).to_s
+        bodyAlert = {
+            "alertingEvents": [{
+                "alertingEventId":""+ae_rand+"",
+                "actionableEntityId": ""+ae_rand+"",
+                "actionableEntityType": "autorizacao_senha",
+                "alertOriginCd": "SID",
+                "alertTypeCd": "AUTORIZACAO",
+                "domainId": "mesa_prev_sky",
+                "score": 1000,
+                "recQueueId": "prev_pf_operation_queue"
+            }],
+            "scenarioFiredEvents": [{
+                "alertingEventId": ""+ae_rand+"",
+                "scenarioFiredEventId": ""+sc_rand+"",
+                "scenarioId": "nomeRegra", #validar
+                "scenarioName": "resposta", #validar
+                "scenarioDeion": "resposta", #validar
+                "scenarioOriginCd": "SID",
+                "displayFlg": "true",
+                "displayTypeCd": "text",
+                "score": 1000
+            }],
+            "enrichment": [{
+                "guia": @solicitation.procedure.guide,
+                "anexo_exame": self.converter_boolean_int(@solicitation.attachment_exam_guide),
+                "anexo_laudo": self.converter_boolean_int(@solicitation.attachment_medical_report),
+                "situacao_financeira_cartao": @solicitation.beneficiary.finantial_status,
+                "situacao_cadastro_cartao": @solicitation.beneficiary.register_status,
+                "situacao_cadastro_referenciado": @solicitation.referenced.register_status,
+                "nome_beneficiario": @solicitation.beneficiary.name,
+                "cartao_beneficiario": @solicitation.beneficiary.card.to_s,
+                "codigo_procedimento": @solicitation.procedure.code.to_so,
+                "descricao_procedimento": @solicitation.procedure.description,
+                "codigo_referenciado": @solicitation.referenced.code.to_s,
+                "cnpj_referenciado": @solicitation.referenced.cnpj_code.to_s,
+                "nome_referenciado": @solicitation.referenced.name,
+                "tabela_procedimento": @solicitation.procedure.table_type,
+                "liberacao_automatica": @solicitation.automatic_release,
+                "analise_adm": @solicitation.adm_analysis,
+                "analise_medica": @solicitation.medic_analysis,
+                "resultado_solicitacao": @solicitation.result
+            }]
+        }
+        puts bodyAlert
     end
 
     def converter_boolean_int(b)
--- sid/analisa_solicitacao.py
+++ sid/analisa_solicitacao.py
@@ -29,7 +29,7 @@ class Solicitacao:
 
 ''' List all output parameters as comma-separated values in the "Output:" docString. Do not specify "None" if there is no output parameter. ''' # noqa
 def execute (guia, anexo_exame, anexo_laudo, situacao_financeira_cartao, situacao_cadastro_cartao, situacao_cadastro_referenciado): # noqa
-    'Output: liberacao_automatica, analise_adm, analise_medica'
+    'Output: liberacao_automatica, analise_adm, analise_medica, resultado_solicitacao'
     s = Solicitacao(guia, anexo_exame, anexo_laudo, situacao_financeira_cartao, situacao_cadastro_cartao, situacao_cadastro_referenciado)
     liberacao_automatica, analise_adm, analise_medica, resultado_solicitacao = s.julga_casos()
     return liberacao_automatica, analise_adm, analise_medica, resultado_solicitacao
