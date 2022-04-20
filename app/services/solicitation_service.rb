0d6d8d47af54efb18c7d7b12c463c9ca0aeda5d9
lucasmoutinho <lucasmoutinho@github.com>
2022-04-20 00:56:34 -0300

    :recycle: basic request form for auth sas

--- app/services/solicitation_service.rb
+++ app/services/solicitation_service.rb
@@ -1,3 +1,5 @@
+require 'httparty'
+
 class SolicitationService
     def initialize(solicitation)
         @solicitation = solicitation
@@ -8,11 +10,31 @@ class SolicitationService
     end
 
     def update_solicitation_api
+        self.call_sas_api
         @solicitation.update(automatic_release: false, adm_analysis: true, medic_analysis: true)
     end
 
-    #def call_sas_api
-    #  @solicitation.procedure.cnpj_code
-    #  @solicitation
-    # end
+    def call_sas_api
+        # parâmetros de entrada SAS
+        baseUrl = "https://server.demo.sas.com"
+        username = "sasdemo"
+        password = "Orion123"
+
+        # Cria uma primeira chamada ao SID para gerar um token de acesso
+        urlToken3 = baseUrl + "/SASLogon/oauth/token"
+        headers3 = {
+            "Content-Type" => "application/x-www-form-urlencoded",
+        }
+        data3 = {
+            "grant_type" => "password",
+            "username" => username,
+            "password" => password
+        }
+        authToken = {:username => "sas.cli", :password => ""}
+
+        token_response = HTTParty.post(urlToken3, :headers => headers3, :body => data3.to_json, :verify => false, :auth => authToken)
+        puts token_response
+        #@solicitation.procedure.cnpj_code
+        #@solicitation
+    end
 end
