ecbc22735e363463c486b71b8006c8d0d98d6cc1
lucasmoutinho <lucasmoutinho@github.com>
2022-04-20 04:25:01 -0300

    :recycle: update: analise_situacao sid

---
+++ sid/analisa_solicitacao.py
@@ -0,0 +1,31 @@
+class Solicitacao:
+    def __init__(self, guia, anexo_exame, anexo_laudo, situacao_financeira_cartao, situacao_cadastro_cartao, situacao_cadastro_referenciado) -> None:
+        self._guia = guia
+        self._anexo_exame = anexo_exame
+        self._anexo_laudo = anexo_laudo
+        self._situacao_financeira_cartao = situacao_financeira_cartao
+        self._situacao_cadastro_cartao = situacao_cadastro_cartao
+        self._situacao_cadastro_referenciado = situacao_cadastro_referenciado
+
+    def julga_casos(self):
+        liberacao_automatica = True
+        analise_adm = True
+        analise_medica = True
+        if(self._guia == 'SADT' and (not self._anexo_exame)):
+            liberacao_automatica = False
+        if(self._guia == 'Internação' and (not self._anexo_laudo)):
+            liberacao_automatica = False
+        if(self._situacao_financeira_cartao == 'Atrasado'):
+            liberacao_automatica = False
+            analise_medica = False
+        if(self._situacao_cadastro_cartao == 'Em auditoria' or self._situacao_cadastro_referenciado == 'Em auditoria'):
+            liberacao_automatica = False
+        return liberacao_automatica, analise_adm, analise_medica
+
+
+''' List all output parameters as comma-separated values in the "Output:" docString. Do not specify "None" if there is no output parameter. ''' # noqa
+def execute (guia, anexo_exame, anexo_laudo, situacao_financeira_cartao, situacao_cadastro_cartao, situacao_cadastro_referenciado): # noqa
+    'Output: liberacao_automatica, analise_adm, analise_medica'
+    s = Solicitacao(guia, anexo_exame, anexo_laudo, situacao_financeira_cartao, situacao_cadastro_cartao, situacao_cadastro_referenciado)
+    liberacao_automatica, analise_adm, analise_medica = s.julga_casos()
+    return liberacao_automatica, analise_adm, analise_medica
--- sid/resolve_json_bradesco.py
+++ sid/resolve_json_bradesco.py
@@ -1,12 +1,18 @@
 import json
 
+def retorna_booleano(number: int):
+    response = False
+    if number == 1:
+        response = True
+    return response
+
 ''' List all output parameters as comma-separated values in the "Output:" docString. Do not specify "None" if there is no output parameter. ''' # noqa
 def execute (json_entrada): # noqa
     'Output: guia, anexo_exame, anexo_laudo, situacao_financeira_cartao, situacao_cadastro_cartao, situacao_cadastro_referenciado'
     j = json.loads(json_entrada)
     guia = j["guia"]
-    anexo_exame = j["anexo_exame"]
-    anexo_laudo = j["anexo_laudo"]
+    anexo_exame = retorna_booleano(j["anexo_exame"])
+    anexo_laudo = retorna_booleano(j["anexo_laudo"])
     situacao_financeira_cartao = j["situacao_financeira_cartao"]
     situacao_cadastro_cartao = j["situacao_cadastro_cartao"]
     situacao_cadastro_referenciado = j["situacao_cadastro_referenciado"]
