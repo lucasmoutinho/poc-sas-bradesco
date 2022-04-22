0c643ea7d8e11d817792cb0f2969d9552bd90dab
Lucas Moutinho <lucas.silvamoutinho@gmail.com>
2022-04-22 10:49:59 -0400

    Update cria_alerta_vi.py

--- sid/cria_alerta_vi.py
+++ sid/cria_alerta_vi.py
@@ -5,11 +5,12 @@ import random
 import datetime
 
 ''' List all output parameters as comma-separated values in the "Output:" docString. Do not specify "None" if there is no output parameter. '''
-def execute (guia, anexo_exame, anexo_laudo, situacao_financeira_cartao, situacao_cadastro_cartao, situacao_cadastro_referenciado, nome_beneficiario, cartao_beneficiario, codigo_procedimento, descricao_procedimento, codigo_referenciado, cnpj_referenciado, nome_referenciado, tabela_procedimento, liberacao_automatica, analise_adm, analise_medica, resultado_solicitacao): # noqa
-    'Output: resposta_vi'
-    host = 'http://13.90.73.61'
+def execute (guia, anexo_exame, anexo_laudo, situacao_financeira_cartao, situacao_cadastro_cartao, situacao_cadastro_referenciado, nome_beneficiario, cartao_beneficiario, codigo_procedimento, descricao_procedimento, codigo_referenciado, cnpj_referenciado, nome_referenciado, tabela_procedimento, liberacao_automatica, analise_adm, analise_medica, resultado_solicitacao, detalhe_resposta):
+    'Output:resposta_vi'
+    
+    host = 'https://tenant5.demoserver.internal.cloudapp.net'
     username = "t5user01"
-    password = "Orion1234"
+    password = "Orion123"
 
     urlToken3 = host + "/SASLogon/oauth/token"
     headers3 = {
@@ -21,7 +22,7 @@ def execute (guia, anexo_exame, anexo_laudo, situacao_financeira_cartao, situaca
         "username": username,
         "password": password
     }
-    authToken =("sas.cli", "")
+    authToken =("sas.ec", "")
 
     tokenFinal = requests.post(urlToken3, data = data3,headers=headers3,verify=False, auth = authToken)    
 
@@ -38,31 +39,33 @@ def execute (guia, anexo_exame, anexo_laudo, situacao_financeira_cartao, situaca
         
     ae_rand= "AE" + str(datetime.datetime.now().timestamp()).split('.')[0] + str(random.randint(17, 999999))
     sc_rand= "SC" + str(datetime.datetime.now().timestamp()).split('.')[0] + str(random.randint(17, 999999))
+    cod_id = str(random.randint(17, 999999))
 
                         
     bodyAlert = {
             "alertingEvents": [{
                 "alertingEventId":""+ae_rand+"",
                 "actionableEntityId": ""+ae_rand+"",
-                "actionableEntityType": "autorizacao_senha",
+                "actionableEntityType": "AutorizacaoSenha",
                 "alertOriginCd": "SID",
                 "alertTypeCd": "AUTORIZACAO",
-                "domainId": "mesa_prev_sky",
-                "score": 1000,
-                "recQueueId": "prev_pf_operation_queue"
+                "domainId": "svidomain",
+                "score": 900,
+                "recQueueId": "AutSenha"
             }],
             "scenarioFiredEvents": [{
                 "alertingEventId": ""+ae_rand+"",
                 "scenarioFiredEventId": ""+sc_rand+"",
-                "scenarioId": "nomeRegra", #validar
-                "scenarioName": "resposta", #validar
-                "scenarioDeion": "resposta", #validar
+                "scenarioId": cod_id,
+                "scenarioDescription": detalhe_resposta,
+                "messageTemplateTxt": detalhe_resposta,
                 "scenarioOriginCd": "SID",
                 "displayFlg": "true",
                 "displayTypeCd": "text",
-                "score": 1000
+                "score": 900
             }],
             "enrichment": [{
+            	"alertingEventId":""+ae_rand+"",
                 "guia": guia,
                 "anexo_exame": anexo_exame,
                 "anexo_laudo": anexo_laudo,
@@ -84,5 +87,6 @@ def execute (guia, anexo_exame, anexo_laudo, situacao_financeira_cartao, situaca
             }]
     }
     resposta_vi = requests.post(urlAlert,data = json.dumps(bodyAlert),headers=headersAlert,verify=False)
+    resposta = str(resposta_vi.json())
 
-    return resposta_vi
+    return resposta
